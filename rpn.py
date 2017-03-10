994f7f0cb4d8736911887de8caa50db3ab659157
Kevin Yang <yangkegit@users.noreply.github.com>
2017-03-10 18:44:39 -0500

    Everything

---
+++ rpn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import operator
+
+Operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(arg):
+    stack = []
+    for operand in arg.split():
+        try:
+            operand = float(operand)
+            stack.append(operand)
+        except:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            stack.append(Operators[operand](arg1, arg2))
+    return stack.pop()
+
+def main():
+    while True:
+        result = calculate(input('rpn calc> '))
+        print("Result:", result)
+
+if __name__ == '__main__':
+    main()
